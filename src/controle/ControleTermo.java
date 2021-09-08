8a91689b5d6d501937ca54fe67ad54acb95581a9
gabrielapinto <gabrielapinto@ufcspa.edu.br>
2021-09-08 16:05:50 -0300

    1605

--- src/controle/ControleTermo.java
+++ src/controle/ControleTermo.java
@@ -5,7 +5,6 @@ import java.awt.event.ActionListener;
 
 import javax.swing.ImageIcon;
 import javax.swing.JOptionPane;
-import javax.swing.JTextField;
 
 import modelo.TermoModel;
 import dao.TermoCompromissoDAO;
@@ -25,8 +24,8 @@ public class ControleTermo implements ActionListener {
 		this.telaTermo = telaTermo;
 		this.TermoModel = TermoModel;
 		
-		this.telaTermo.getTextFieldCpfAluno().addActionListener(this);;
-		this.telaTermo.getTextFieldCnpjEmpresa().addActionListener(this);
+		this.telaTermo.getFormattedTextFieldCpfAluno().addActionListener(this);
+		this.telaTermo.getFormattedTextFieldCnpjEmpresa().addActionListener(this);
 		this.telaTermo.getBtnCadastrar().addActionListener(this);
 		
 		this.janelaPrincipal = janelaPrincipal;
@@ -44,13 +43,11 @@ public class ControleTermo implements ActionListener {
 			this.janelaPrincipal.getTermo().limparTermo();
 		} 
 
-		if(e.getSource() == this.telaTermo.getTextFieldCpfAluno()) {
+		if(e.getSource() == telaTermo.getFormattedTextFieldCpfAluno()) {
 			
-			/*consultaCPF substitui pelo nome do método q vai estar no DAO para fazer busca do CPF*/
-			
-			
-			if(!TermoCompromissoDAO.comparaCPF(this.telaTermo.getTextFieldCpfAluno().getText()))
+			if(!TermoCompromissoDAO.comparaCPF(this.telaTermo.getFormattedTextFieldCpfAluno().getText()))
 				JOptionPane.showMessageDialog(telaTermo, "CPF nÃo cadastrado", "Erro", JOptionPane.ERROR_MESSAGE, new ImageIcon("/icones_logos/refresh (1).png"));
+		
 			else{
 				this.telaTermo.getTextFieldNomeAluno().setText(TermoModel.getNomeAluno());
 				this.telaTermo.getTextFieldCurso().setText(TermoModel.getNomeCurso());
@@ -60,12 +57,10 @@ public class ControleTermo implements ActionListener {
 			}
 		}
 		
-		if(e.getSource() == this.telaTermo.getTextFieldCnpjEmpresa()) {
-			
-			/*consultaCNPJ substitui pelo nome do método q vai estar no DAO para fazer busca do CNPJ*/
+		if(e.getSource() == this.telaTermo.getFormattedTextFieldCnpjEmpresa()) {
 			
 			
-			if(!TermoCompromissoDAO.comparaCNPJ(this.telaTermo.getTextFieldCnpjEmpresa().getText()))
+			if(!TermoCompromissoDAO.comparaCNPJ(this.telaTermo.getFormattedTextFieldCnpjEmpresa().getText()))
 				JOptionPane.showMessageDialog(telaTermo, "CNPJ nÃo cadastrado", "Erro", JOptionPane.ERROR_MESSAGE, new ImageIcon("/icones_logos/refresh (1).png"));
 			
 			else{
@@ -81,25 +76,26 @@ public class ControleTermo implements ActionListener {
 				|| this.telaTermo.getTextFieldBeneficios().getText().isEmpty()
 				|| this.telaTermo.getTextFieldChDiaria().getText().isEmpty()
 				|| this.telaTermo.getTextFieldChSemanal().getText().isEmpty()
-				|| this.telaTermo.getTextFieldCnpjEmpresa().getText().isEmpty()
-				|| this.telaTermo.getTextFieldCpfAluno().getText().isEmpty()
+				|| this.telaTermo.getFormattedTextFieldCnpjEmpresa().getText().isEmpty()
+				|| this.telaTermo.getFormattedTextFieldCpfAluno().getText().isEmpty()
 				|| this.telaTermo.getTextFieldCurso().getText().isEmpty()
-				|| this.telaTermo.getTextFieldFim().getText().isEmpty()
-				|| this.telaTermo.getTextFieldHorarioDeFim().getText().isEmpty()
-				|| this.telaTermo.getTextFieldHorarioDeInicio().getText().isEmpty()
+				|| this.telaTermo.getFormattedTextFieldFim().getText().isEmpty()
+				|| this.telaTermo.getFormattedTextFieldHorarioDeFim().getText().isEmpty()
+				|| this.telaTermo.getFormattedTextFieldHorarioDeInicio().getText().isEmpty()
 				|| this.telaTermo.getTextFieldInfoComplementares().getText().isEmpty()
-				|| this.telaTermo.getTextFieldInicio().getText().isEmpty()
+				|| this.telaTermo.getFormattedTextFieldInicio().getText().isEmpty()
 				|| this.telaTermo.getTextFieldNomeAluno().getText().isEmpty()
 				|| this.telaTermo.getTextFieldOrientador().getText().isEmpty()
 				|| this.telaTermo.getTextFieldRazaoSocial().getText().isEmpty()
 				|| this.telaTermo.getTextFieldRepresentanteEmpresa().getText().isEmpty()
 				|| this.telaTermo.getTextFieldRepresentanteUniversidade().getText().isEmpty()
 				|| this.telaTermo.getTextFieldUniversidade().getText().isEmpty()
-				|| this.telaTermo.getTextFieldValor().getText().isEmpty()){
+				|| this.telaTermo.getFormattedTextFieldValor().getText().isEmpty()){
 				
 				JOptionPane.showMessageDialog(telaTermo, "Cadastro não efetuado, todos os campos precisam ser preenchidos!", "Erro", JOptionPane.ERROR_MESSAGE, new ImageIcon("/icones_logos/refresh (1).png"));
 			
 			}else{
+			
 				
 				TermoModel= new TermoModel();
 				
@@ -108,24 +104,22 @@ public class ControleTermo implements ActionListener {
 				TermoModel.setBeneficios(this.telaTermo.getTextFieldBeneficios().getText());
 				TermoModel.setChDiaria(Integer.parseInt(this.telaTermo.getTextFieldChDiaria().getText()));
 				TermoModel.setChSemanal(Integer.parseInt(this.telaTermo.getTextFieldChSemanal().getText()));
-				TermoModel.setCnpjConcedente(this.telaTermo.getTextFieldCnpjEmpresa().getText());
-				TermoModel.setCpfAluno(this.telaTermo.getTextFieldCpfAluno().getText());
+				TermoModel.setCnpjConcedente(this.telaTermo.getFormattedTextFieldCnpjEmpresa().getText());
+				TermoModel.setCpfAluno(this.telaTermo.getFormattedTextFieldCpfAluno().getText());
 				TermoModel.setNomeCurso(this.telaTermo.getTextFieldCurso().getText());
-				TermoModel.setFimFstagio(this.telaTermo.getTextFieldFim().getText());
-				TermoModel.setHoraFimAtividadesEstagio(this.telaTermo.getTextFieldHorarioDeFim().getText());
-				TermoModel.setHoraInicioAtividadesEstagio(this.telaTermo.getTextFieldHorarioDeInicio().getText());
+				TermoModel.setFimFstagio(this.telaTermo.getFormattedTextFieldFim().getText());
+				TermoModel.setHoraFimAtividadesEstagio(this.telaTermo.getFormattedTextFieldHorarioDeFim().getText());
+				TermoModel.setHoraInicioAtividadesEstagio(this.telaTermo.getFormattedTextFieldHorarioDeInicio().getText());
 				//TermoModel.set(this.telaTermo.getTextFieldInfoComplementares().getText());
-				TermoModel.setInicioEstagio(this.telaTermo.getTextFieldInicio().getText());
+				TermoModel.setInicioEstagio(this.telaTermo.getFormattedTextFieldInicio().getText());
 				TermoModel.setNomeAluno(this.telaTermo.getTextFieldNomeAluno().getText());
 				TermoModel.setNomeOrientador(this.telaTermo.getTextFieldOrientador().getText());
 				TermoModel.setRazaSocialEmpresaConcedente(this.telaTermo.getTextFieldRazaoSocial().getText());
 				TermoModel.setNomeRepresentanteEmpresaConcedenteEstagio(this.telaTermo.getTextFieldRepresentanteEmpresa().getText());
 				TermoModel.setNomeRepresentanteUniversidade(this.telaTermo.getTextFieldRepresentanteUniversidade().getText());
 				TermoModel.setNomeUniversidade(this.telaTermo.getTextFieldUniversidade().getText());
-				TermoModel.setValorBolsa(Integer.parseInt(this.telaTermo.getTextFieldValor().getText()));
+				TermoModel.setValorBolsa(Integer.parseInt(this.telaTermo.getFormattedTextFieldValor().getText()));
 				
-				
-				//substitui cadastrar pelo metodo no DAO que realiza o cadastro
 				
 				TermoCompromissoDAO.cadastrar(TermoModel);
 				JOptionPane.showMessageDialog(telaTermo, "Cadastro efetuado com sucesso!", "Cadastro", JOptionPane.INFORMATION_MESSAGE, new ImageIcon(JanelaPrincipal.class.getResource("/icones_logos/check.png")));
